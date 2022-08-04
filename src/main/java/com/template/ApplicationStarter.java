92011fba323760e538b5b49e14a5cd4b6cf7b325
khshanovskyi <khshanovskyi@gmail.com>
2022-08-04 12:24:13 +0300

    Initial commit

---
+++ src/main/java/com/template/ApplicationStarter.java
@@ -0,0 +1,11 @@
+package com.template;
+
+/**
+ * This is the start endpoint of the application.
+ */
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        AppContextFactory.getContext();
+    }
+}
